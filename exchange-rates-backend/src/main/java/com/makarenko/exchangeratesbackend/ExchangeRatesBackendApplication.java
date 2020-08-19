8b1c228cf5e5b1126e2212847b77dbce1cc9dc79
Makar <makar@Makars-MacBook-Pro.local>
2020-08-19 18:45:37 +0300

    first commit - project structure

---
+++ exchange-rates-backend/src/main/java/com/makarenko/exchangeratesbackend/ExchangeRatesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.makarenko.exchangeratesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRatesBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExchangeRatesBackendApplication.class, args);
+  }
+
+}
