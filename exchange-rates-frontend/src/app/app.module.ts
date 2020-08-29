08865cf90596f8fbcd28c0c19e1cc7a36aa5f61d
Makar <makar@Makars-MacBook-Pro.local>
2020-08-29 10:18:36 +0300

    frontend part make

--- exchange-rates-frontend/src/app/app.module.ts
+++ exchange-rates-frontend/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import {authInterceptorProviders} from './_helpers/auth.interceptor';
+import {HttpClientModule} from '@angular/common/http';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    BoardUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
